43731f90d138d920edf1e4ef6bf1000c0644ef3d
Ismail Mehdi <ismailmehdi@google.com>
2026-03-25 18:49:38 +0000

    feat: add UV_NO_SYNC support to run script and update Dockerfile and cloudbuild configuration accordingly

--- evalbench/run.sh
+++ evalbench/run.sh
@@ -10,7 +10,9 @@ ulimit -n 4096
 
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
-if command -v uv &> /dev/null; then
+if [[ "$UV_NO_SYNC" == "true" ]]; then
+  python evalbench/evalbench.py --experiment_config="$EVAL_CONFIG"
+elif command -v uv &> /dev/null; then
   uv run evalbench/evalbench.py --experiment_config="$EVAL_CONFIG"
 else
   python evalbench/evalbench.py --experiment_config="$EVAL_CONFIG"
